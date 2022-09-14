0ec41937c422298da2a801b7d6be80475a9a6f56
Andrey Ilinov <andrey.ilinov@quadcode.com>
2022-09-14 13:33:47 +0300

    hw03: top10

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"bufio"
+	"math"
+	"sort"
+	"strings"
+)
+
+const topCount = 10
+
+type wordCount struct {
+	word  string
+	count int
+}
+
+type wordsCountList []wordCount
+
+func Top10(in string) (out []string) {
+	occurrences := make(map[string]int)
+	r := strings.NewReader(in)
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+	for s.Scan() {
+		key := strings.ToLower(strings.Trim(s.Text(), "!.,-?:\"()"))
+		if key != "" {
+			occurrences[key]++
+		}
+	}
+	wordsCount := make(wordsCountList, len(occurrences))
+	i := 0
+	for w, c := range occurrences {
+		wordsCount[i] = wordCount{w, c}
+		i++
+	}
+	sort.Slice(wordsCount, sortByCount(wordsCount))
+
+	for _, wc := range wordsCount[:int(math.Min(float64(len(wordsCount)), topCount))] {
+		out = append(out, wc.word)
+	}
+
+	return out
+}
+
+func sortByCount(wcl wordsCountList) func(i int, j int) bool {
+	return func(i, j int) bool {
+		if wcl[i].count == wcl[j].count {
+			return wcl[i].word < wcl[j].word
+		}
+		return wcl[i].count > wcl[j].count
+	}
 }
