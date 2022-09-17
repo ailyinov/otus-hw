8c63daccca3eab865cb602aa9826d74d312686fb
Andrey Ilinov <andrey.ilinov@quadcode.com>
2022-09-17 21:16:40 +0300

    hw04: lru cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -3,14 +3,12 @@ package hw04lrucache
 type Key string
 
 type Cache interface {
-	Set(key Key, value interface{}) bool
-	Get(key Key) (interface{}, bool)
+	Set(key Key, value any) bool
+	Get(key Key) (any, bool)
 	Clear()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -26,5 +24,43 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	}
+}
+
+func (l *lruCache) Set(key Key, value any) bool {
+	ci := &cacheItem{
+		key:   key,
+		value: value,
+	}
+	_, ok := l.items[key]
+	if ok {
+		l.items[key].Value = ci
+		l.queue.MoveToFront(l.items[key])
+	} else {
+		l.assureCapacity()
+		l.items[key] = l.queue.PushFront(ci)
+		l.capacity--
+	}
+	return ok
+}
+
+func (l *lruCache) Get(key Key) (any, bool) {
+	if i, ok := l.items[key]; ok {
+		l.queue.MoveToFront(i)
+		return i.Value.(*cacheItem).value, true
+	}
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.queue = NewList()
+	l.items = make(map[Key]*ListItem, l.capacity)
+}
+
+func (l *lruCache) assureCapacity() {
+	if l.capacity == 0 {
+		delete(l.items, l.queue.Back().Value.(*cacheItem).key)
+		l.queue.Remove(l.queue.Back())
+		l.capacity++
 	}
 }
