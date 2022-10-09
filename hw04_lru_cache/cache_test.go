2df0c8e64ab2166dc7841fca96e8bc76824c0c09
Andrey Ilinov <andrey.ilinov@quadcode.com>
2022-10-09 20:09:01 +0300

    hw04: fit test timeout

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -132,15 +132,15 @@ func TestCacheMultithreading(t *testing.T) {
 
 		go func() {
 			defer wg.Done()
-			for i := 0; i < 1_000_0000; i++ {
+			for i := 0; i < 1_000_000; i++ {
 				c.Set(Key(strconv.Itoa(i)), i)
 			}
 		}()
 
 		go func() {
 			defer wg.Done()
-			for i := 0; i < 1_000_0000; i++ {
-				c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+			for i := 0; i < 1_000_000; i++ {
+				c.Get(Key(strconv.Itoa(rand.Intn(1_000_00))))
 			}
 		}()
 
@@ -190,15 +190,15 @@ func TestCacheMultithreading(t *testing.T) {
 
 		go func() {
 			defer wg.Done()
-			for i := 0; i < 1_000_0000; i++ {
+			for i := 0; i < 1_000_000; i++ {
 				c.Set(Key(strconv.Itoa(i)), i)
 			}
 		}()
 
 		go func() {
 			defer wg.Done()
-			for i := 0; i < 1_000_0000; i++ {
-				c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+			for i := 0; i < 1_000_000; i++ {
+				c.Get(Key(strconv.Itoa(rand.Intn(1_000_00))))
 			}
 		}()
 
