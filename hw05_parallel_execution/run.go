3743abbecb43694bb6831aecc2151b872b118580
Andrey Ilinov <andrey.ilinov@quadcode.com>
2022-10-17 22:08:25 +0300

    hw05: parallel tasks

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,6 +1,7 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
 )
 
@@ -8,8 +9,44 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func worker(jobs <-chan Task, results chan<- error, ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			for j := range jobs {
+				results <- j()
+			}
+		}
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	jobs := make(chan Task, n)
+	results := make(chan error, len(tasks))
+	defer close(jobs)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
+
+	for w := 0; w < n; w++ {
+		go worker(jobs, results, ctx)
+	}
+
+	for _, t := range tasks {
+		jobs <- t
+	}
+
+	errsCount := 0
+	for k := 0; k <= len(tasks); k++ {
+		if err := <-results; err != nil {
+			if errsCount++; errsCount >= m {
+				return ErrErrorsLimitExceeded
+			}
+		}
+	}
+
 	return nil
 }
